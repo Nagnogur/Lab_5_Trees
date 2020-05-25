033473264f250975bcf681b491dc2a84ca3d5028
Nagnogur <trostinetsky.sasha1@gmail.com>
2020-05-25 18:45:38 +0300

    Added variable node

---
+++ Lab_5/VariableNode.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_5
+{
+    class VariableNode
+    {
+        string type = "Variable";
+        string value;
+        public string Value
+        {
+            get { return value; }
+            set { this.value = value; }
+        }
+
+        public VariableNode(string val)
+        {
+            value = val;
+        }
+    }
+}
